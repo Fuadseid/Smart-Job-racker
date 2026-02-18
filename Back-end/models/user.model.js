c3c31061cc28b984078df707f50ee57d6a9d7234
Fuadseid <fuaddbus@gmail.com>
2026-02-18 16:44:21 +0300

    add the user schema

---
+++ Back-end/models/user.model.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bycrupt = require('bcrypt')
+const ApiError = require("../utils/ApiError");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new ApiError(400, "Invalid email format");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    validate(value) {
+      if (!validator.isStrongPassword(value)) {
+        throw new ApiError(
+          400,
+          "Password should contain at least one uppercase letter, one lowercase letter, one number, and one special character",
+        );
+      }
+    },
+  },
+  
+});
+
+userSchema.statics.isEmailTaken = async function (email) {
+  const user = await this.findOne({ email });
+  return !!user;
+};
+
+userSchema.pre('save',async function(){
+  const user = this;
+   if (user.isModified('password')){
+     user.password = await bcrypt.hash(user.password,8)
+   }
+})
+
+userSchema.methods.isPasswordMatch = async function(password){
+const user = this
+return await bcrypt.compare(password,user.password)
+}
+
+userSchema.plugin(tojson)
+
+
+module.exports = mongoose.model("user", userSchema);
