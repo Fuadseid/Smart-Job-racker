558af343d0f3ff595905d769afa32fda110733cf
Fuadseid <fuaddbus@gmail.com>
2026-02-26 14:28:36 +0300

    smoothen the scroll for intractivity

--- Front-end/src/pages/_app.js
+++ Front-end/src/pages/_app.js
@@ -3,11 +3,15 @@ import { Provider } from "react-redux";
 import { store } from "@/store/store";
 
 import WrapperComponent from "@/wrapper";
+import SmoothScroll from "@/components/SmoothScroll";
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      
         <WrapperComponent>
+          <SmoothScroll>
           <Component {...pageProps} />
+          </SmoothScroll>
         </WrapperComponent>
     </Provider>
   );
